9dc185b93b8a43625f96bf7dc71ffafb9af8432d
Swarnalatha Selvanathan <swarna@macbook-pro-8.home>
2020-04-21 12:29:33 +0100

    changed the AMi Variable name

--- dev/build.sh
+++ dev/build.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 packer build packer.json 2>&1 | tee build.txt
-AMI=$(tail -2 build.txt | head -2 | awk 'match($0, /ami-.*/) { print substr($0, RSTART, RLENGTH) }')
-printf "%s" "$NGINX_AMI" > AMI.txt
+NGINX_AMI=$(tail -2 build.txt | head -2 | awk 'match($0, /ami-.*/) { print substr($0, RSTART, RLENGTH) }')
+printf "%s" "$NGINX_AMI" > NGINX_AMI.txt
 rm build.txt
-TF_VAR_AMI_ID=$(cat AMI.txt) terraform apply  
+TF_VAR_AMI_ID=$(cat NGINX_AMI.txt) terraform apply  
