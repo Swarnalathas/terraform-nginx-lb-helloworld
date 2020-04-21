d4ed26718647c58dc6453f9f93310e22b1f0108f
Swarnalatha Selvanathan <swarna@MacBook-Pro-8.local>
2020-04-21 10:32:59 +0100

    updated the Nginx config

--- dev/main.tf
+++ dev/main.tf
@@ -8,13 +8,30 @@ provider "aws" {
 }
 
 resource "aws_instance" "nginx" {
-  ami           = "ami-08d8cabe75ef22b4e"
+  ami           = "aws_ami.id"
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.nginx_aws_sg.id]
 
   tags = {
     Name = "Nginx-Load-Balancer"
   }
+  # Events   
+  #   {   
+  #     worker_connections 768;   
+  #   } 
+  # http {
+  #   upstream myproject {
+  #   server 127.0.0.1:8000 weight=1;
+  #   server 127.0.0.1:8001 weight=1;   
+  # }
+
+  # server {
+  #   listen 80;
+  #   server_name www.domain.com;
+  #   location / {
+  #     proxy_pass http://myproject;
+  #   }
+  # }
 }
 
 resource "aws_security_group" "nginx_aws_sg" {
@@ -28,32 +45,73 @@ resource "aws_security_group" "nginx_aws_sg" {
   }
 }
 
-resource "aws_instance" "swsel_app" {
-  ami           = "ami-006a0174c6c25ac06"
-  instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.swsel_asg.id]
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World!! ELB Testing" > index.html
-              nohup busybox httpd -f -p 8080 &
-              EOF
-  tags = {
-    Name = "swsel_app"
+data "aws_ami" "example" {
+  executable_users = ["self"]
+  most_recent      = true
+  name_regex       = "^myami-\\d{3}"
+  owners           = ["self"]
+
+  filter {
+    name   = "name"
+    values = ["myami-*"]
   }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+# resource "aws_instance" "server_A" {
+#   ami           = "ami-006a0174c6c25ac06"
+#   instance_type = "t2.micro"
+#   vpc_security_group_ids = [aws_security_group.swsel_asg.id]
+#   user_data = <<-EOF
+#               #!/bin/bash
+#               echo "Hello, World!! Server A" > index.html
+#               nohup busybox httpd -f -p 8080 &
+#               EOF
+#   tags = {
+#     Name = "server_A"
+#   }
   
-  lifecycle {
-    create_before_destroy = true
-  }
-}
+#   lifecycle {
+#     create_before_destroy = true
+#   }
+# }
+# resource "aws_instance" "server_B" {
+#   ami           = "ami-006a0174c6c25ac06"
+#   instance_type = "t2.micro"
+#   vpc_security_group_ids = [aws_security_group.swsel_asg.id]
+#   user_data = <<-EOF
+#               #!/bin/bash
+#               echo "Hello, World!! Server B" > index.html
+#               nohup busybox httpd -f -p 8080 &
+#               EOF
+#   tags = {
+#     Name = "server_B"
+#   }
+  
+#   lifecycle {
+#     create_before_destroy = true
+#   }
+# }
 
-resource "aws_security_group" "swsel_asg" {
-  name = "swsel-terraform-asg-instance"
+# resource "aws_security_group" "swsel_asg" {
+#   name = "swsel-terraform-asg-instance"
 
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+#   ingress {
+#     from_port   = 8080
+#     to_port     = 8080
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
+
+
 
